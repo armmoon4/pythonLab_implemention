d2adb3728fe14dbe38d0cc8d3b18aa3d94b41b98
armmoon4 <rahman15-4343@diu.edu.bd>
2023-02-21 01:59:10 +0600

    Update

---
+++ Function/labPractice2.py
@@ -0,0 +1,6 @@
+#labpractice >>>Take your nick name(such as 'mufty') and last part of your student ID (such as '13329') your varsity mail
+#output format mufty15-1222@diu.edu.bd
+
+name=(input("Enter your name:"))
+id=str(input("Enter your id:"))
+print(name,id,"@diu.edu.bd")
