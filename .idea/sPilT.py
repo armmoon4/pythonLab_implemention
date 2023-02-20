dd65d34b927a2dec16c29c68e5d93382df75e565
armmoon4 <rahman15-4343@diu.edu.bd>
2023-02-21 01:35:10 +0600

    Update

---
+++ .idea/sPilT.py
@@ -0,0 +1,18 @@
+txt = "welcome to the jungle"
+
+x = txt.split()
+
+print(x)
+
+txt = "hello, my name is Peter, I am 26 years old"
+
+x = txt.split(", ")
+
+print(x)
+
+txt = "apple#banana#cherry#orange"
+
+# setting the maxsplit parameter to 1, will return a list with 2 elements!
+x = txt.split("#", 1)
+
+print(x)
