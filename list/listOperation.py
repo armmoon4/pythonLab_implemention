ea2c130ef2634f0d2d3a75958f07a340fd0751a5
armmoon4 <rahman15-4343@diu.edu.bd>
2023-02-26 17:35:35 +0600

    Update

--- list/listOperation.py
+++ list/listOperation.py
@@ -8,3 +8,16 @@ print(lst0)
 
 lst0.insert(2 , "pandas")  #insertion in specific positin
 print(lst0)
+
+lst0.remove(3)  #remove in specific position
+print(lst0)
+
+print(lst0.index("python"))  #reversing of the list
+print(lst0.count('pandas'))
+print(lst0.reverse())
+
+lst1=lst0.copy()
+print(lst1)             #reversing
+
+lst0.clear()       #empty list
+print(lst0)
