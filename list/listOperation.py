29441a2b904097c46b33941a35a6add83fc5f70c
armmoon4 <rahman15-4343@diu.edu.bd>
2023-02-26 17:32:50 +0600

    Update

--- list/listOperation.py
+++ list/listOperation.py
@@ -3,5 +3,8 @@
 lst0=[1,2,3,32,2.32,43.23 , "python", [2,32,3]]
 print(len(lst0))
 
-lst0.append([12,2,12,2])
+lst0.append([12,2,12,2])  #add in last of the last
+print(lst0)
+
+lst0.insert(2 , "pandas")  #insertion in specific positin
 print(lst0)
