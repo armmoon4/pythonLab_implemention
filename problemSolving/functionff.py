f2b380d166c7bf036699cc1af3939ec5876e84e1
armmoon4 <rahman15-4343@diu.edu.bd>
2023-02-27 19:03:44 +0600

    Update

---
+++ problemSolving/functionff.py
@@ -0,0 +1,14 @@
+print('Hi, i am semicolon do you miss me?')
+print('Let me know if you miss me')
+s=input()
+a=10
+b=32
+print(max(a,b))
+print(min(a,b))
+
+c = 10.33
+d=int(c)
+print(d)
+
+e = "python"
+print(type(e))
