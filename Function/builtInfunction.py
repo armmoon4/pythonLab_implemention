6a67ffa8129b53a09913ee72426f35217ae292ee
armmoon4 <rahman15-4343@diu.edu.bd>
2023-02-27 19:17:03 +0600

    Update

---
+++ Function/builtInfunction.py
@@ -0,0 +1,17 @@
+#built-in functions
+
+print("Hi, I am semi-colon. Do you miss me?")
+print("Let me know if you miss me or not. ")
+s=input()
+
+a=10
+b=21
+print(max(a,b))
+print(min(a,b))
+
+c=10.4
+d=int(c)
+print(d)
+
+e="python"
+print(type(e))
