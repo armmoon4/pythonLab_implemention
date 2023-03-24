ee056b65558249a048fb9d8676a233e5d4ed3748
armmoon4 <rahman15-4343@diu.edu.bd>
2023-03-24 15:16:01 +0600

    Update

--- pythonSequence/task10.py
+++ pythonSequence/task10.py
@@ -1,4 +1,4 @@
-book_genres = {'sci fi': 12, 'mystery': 15, 'horror': 8, 'mythology': 10, 'young_adult': 4, 'adventure':14}
+book_genres = {'sci fi': 12, 'mystery': 17, 'horror': 8, 'mythology': 10, 'young_adult': 4, 'adventure':14}
 max_value = 0
 max_key = ''
 for key, value in book_genres.items():
