bb7bbe9a9f3b92beef14545031d056056fa40968
armmoon4 <rahman15-4343@diu.edu.bd>
2023-03-24 23:16:22 +0600

    Update

---
+++ controlStatement/whileStatement.py
@@ -0,0 +1,5 @@
+number = 1
+
+while (number<10):
+  print(number, end=" ") #print in same line with space using end =" "
+  number+=1
--- pythonSequence/task7.py
+++ pythonSequence/task7.py
@@ -3,3 +3,6 @@ dict2 = {'Ginie': 18, 'Luna': 14}
 marks = dict(dict1)
 marks.update(dict2)
 print(marks)
+jio = [5 , 6 ,7 , 9 ]
+jio.insert(2 , 3)
+print(jio)
