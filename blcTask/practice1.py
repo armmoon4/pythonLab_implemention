910e35ad3aa502fd735bda5dce6117acb18204e5
armmoon4 <rahman-15-4343@diu.edu.bd>
2023-04-02 16:19:10 +0600

    Update

--- blcTask/practice1.py
+++ blcTask/practice1.py
@@ -1,23 +1,13 @@
 i=1
 
 while i<=8:
-
   i=i+1
-
   a=input("Enter the ID:")
-
   m1 = int (input("Enter the marks in the first subject: "))
-
   m2 = int (input("Enter the marks in the second subject: "))
-
   m3 = int (input("Enter the marks in the third subject: "))
-
   if m1<100 and m2<100 and m3<100:
-
    total=m1+m2+m3
-
    average=total/3
-
    print("ID:",a)
-
    print("Avarage mark=",average)
