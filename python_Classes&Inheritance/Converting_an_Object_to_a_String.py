f10ecefd8bb540a0a1b129fed6fbd52003146d76
Abdur Rahman Moon <rahman15-4343@diu.edu.bd>
2023-05-01 20:25:13 +0600

    Update

---
+++ python_Classes&Inheritance/Converting_an_Object_to_a_String.py
@@ -0,0 +1,62 @@
+"""
+class Point:
+    #Point class for representing and manipulating x,y coordinates.
+
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+
+p = Point(7,6)
+print(p)
+"""
+
+""" 
+class Point:
+     #Point class for representing and manipulating x,y coordinates. 
+
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def __str__(self):
+        return "x = {}, y = {}".format(self.x, self.y)
+
+p = Point(7,6)
+print(p)
+"""
+
+class Cereal:
+    def __init__(self, name, brand, fiber):
+        self.name = name
+        self.brand = brand
+        self.fiber = fiber
+
+    def __str__(self):
+        return "{} cereal is produced by {} and has {} grams of fiber in every serving!".format(self.name, self.brand, self.fiber)
+
+c1 = Cereal("Corn Flakes", "Kellogg's", 2)
+c2 = Cereal("Honey Nut Cheerios", "General Mills", 3)
+
+print(c1)
+print(c2)
