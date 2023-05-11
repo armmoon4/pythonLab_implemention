06c1be9eb46fb541b78441952b215561b2a6a961
Abdur Rahman Moon <rahman15-4343@diu.edu.bd>
2023-05-11 20:22:49 +0600

    Update

---
+++ python_Classes&Inheritance/Overriding_Methods.py
@@ -0,0 +1,24 @@
+class Book:
+    def __init__(self , title , author):
+        self.title = title
+        self.author = author
+    def __str__(self):
+        return '"{}" by {}'.format(self.title , self.author)
+
+class PaperBook(Book):
+    def __init__(self , title , author , numPages):
+        Book.__init__(self , title , author)
+        self.numPages = numPages
+
+class EBook(Book):
+    def __init__(self , title , author , size):
+        Book.__init__(self , title , author)
+        self.size = size
+
+
+Tbook = Book('The Odyssey' , 'Homer')
+myBook = EBook('The Odyssey' , 'Homer' , 2)
+myPaperBook = PaperBook('The Odyssey' , 'Homer' , 500)
+print(Tbook)
+print(myBook.size)
+print(myPaperBook.numPages)
