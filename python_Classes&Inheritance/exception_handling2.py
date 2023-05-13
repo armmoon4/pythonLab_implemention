3fe287935615cb66e2d42567051ca022033bd27d
Abdur Rahman Moon <rahman15-4343@diu.edu.bd>
2023-05-13 20:14:31 +0600

    Update

---
+++ python_Classes&Inheritance/exception_handling2.py
@@ -0,0 +1,18 @@
+try:
+    items = ['a', 'b']
+    third = items[2]
+    print("This won't print")
+except IndexError:
+    print("error 1")
+
+print("continuing")
+
+try:
+    x = 5
+    y = x/0
+    print("This won't print, either")
+except IndexError:
+    print("error 2")
+
+
+print("continuing again")
